e443164ba8e64a82ad7d1e463b387d8f1e61cdb0
Ruben Genao <rubengenao@Rubens-MBP.lan>
2022-07-21 20:29:59 -0400

    Final Push: Project is Done :)

--- app.py
+++ app.py
@@ -1,15 +1,52 @@
-from flask import Flask, render_template, url_for, flash, redirect, request
+from flask import Flask, render_template, url_for, redirect
+from sqlalchemy import false
 from forms import RegistrationForm
 from stockinfo import getinfo, getlabels, getvalues
 from newsdata import getnews
+from forms import LoginForm, RegistrationForm, StockSearchForm
+from flask_behind_proxy import FlaskBehindProxy
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import current_user, LoginManager, login_user, UserMixin, logout_user
+import secrets
+
+
+app = Flask(__name__)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+###Changes made###
+proxied = FlaskBehindProxy(app)
+app.config['SECRET_KEY'] = secrets.token_hex(16)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 
 '''
-y finance, flask, flask wtf, newsdataapi, these are the required imports
+y finance, flask, flask wtf, newsdataapi, these are the required imports, form ,flask_behind_proxy, email_validator, flask_login
 '''
+
+
+db = SQLAlchemy(app)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+class Pref(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=False, nullable=False)
+    stockticker = db.Column(db.String(6), unique=True, nullable = False)
+
+    def __repr__(self):
+        return f"User('{self.stockticker}')"
+
+db.create_all()
 
 def getpopularstocks(stocklist, timeperiod, timeinterval = ''):
     stocksinfo = []
-    stocks = stocklist
+    stocks = set(stocklist)
     for stock in stocks:
         stockdict = {}
         priceaction = getvalues(stock, timeperiod, timeinterval)
@@ -26,31 +63,45 @@ def getpopularstocks(stocklist, timeperiod, timeinterval = ''):
         stocksinfo.append(stockdict)
     return stocksinfo
 
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = '2c27df9a9141cf1529a88ca4da79adf3'
+#--------------------------------------------------------------------------------------------------------------------------#
+'''
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+'''
+@login_manager.user_loader
+def get_user(id):
+  return User.query.get(int(id))
 
 @app.route('/')
 def index():
     index = getpopularstocks(['^IXIC', '^GSPC', '^DJI'], '1d', '1m')
-    stocks = getpopularstocks(['NFLX', 'AAPL', 'AMC', 'GME'], '1d', '1m')
+    ticker = ['NFLX', 'AAPL', 'AMC', 'GME']
+
+    if current_user.is_authenticated:
+        for x in Pref.query.filter_by(username = current_user.username).all():
+            ticker.append(x.stockticker)
+   
+    stocks = getpopularstocks(ticker, '1d', '1m')
     news = getnews()
-    return render_template('home.html', majorindexes = index, stocklists = stocks, stocknews = news)
+    return render_template('home.html', majorindexes = index, stocklists = stocks, stocknews = news, status = current_user.is_authenticated)
 
 @app.route('/stocksearch.html', methods = ["POST", "GET"])
 def search():
-    form = RegistrationForm()
+    form = StockSearchForm()
     
     if form.validate_on_submit():
-        print("hi")
-        #flash(f"Search on {form.ticker.data} was successful!", 'success')
         ticker = form.ticker.data.upper()
+        period = form.timeperiod.data
+        interval = form.timeinterval.data
+
+         
         info = getinfo(ticker)
 
         #TODO: Add time span and interval
 
-        labels = getlabels(ticker, '1mo')
-        values = getvalues(ticker, '1mo')
+        labels = getlabels(ticker, period, interval)
+        values = getvalues(ticker, period, interval)
 
         trend = ''
         if values[0] <= values[len(values) - 1]:
@@ -61,25 +112,65 @@ def search():
         percentchange = ((values[len(values) - 1] - values[0]) / values[0]) * 100
         pricechange = values[len(values) - 1] - values[0]
 
+        if current_user.is_authenticated and Pref.query.filter_by(stockticker = ticker).all() == []:
+            newpref = Pref(username = current_user.username, stockticker = ticker)
+            db.session.add(newpref)
+            db.session.commit()
+
         return render_template('stocksearch.html', form = form, infodict = info, labels = labels, values = values, 
-                                trend = trend, percentchange = percentchange, pricechange = pricechange)        
+                                trend = trend, percentchange = percentchange, pricechange = pricechange, status = current_user.is_authenticated)        
     
     return render_template('stocksearch.html', form = form)
 
-@app.route('/register.html')
+@app.route("/register.html", methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    form = RegistrationForm()
+    if form.validate_on_submit(): 
+        if current_user.is_authenticated == False:
+            user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+            db.session.add(user)
+            db.session.commit()
 
-@app.route('/login.html')
+            return redirect(url_for('home')) 
+    
+
+    return render_template('register.html', title='Register', form=form, status = current_user.is_authenticated)
+
+
+@app.route('/login.html', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        if current_user.is_authenticated == False:
+            user = User.query.filter_by(username = form.username.data, password = form.password.data).first()
+            db = User.query.all()
+            if user != None:
+                login_user(user)
+                return redirect(url_for('home'))
+
+    return render_template('login.html', title="Login", form=form, status = current_user.is_authenticated)
 
 @app.route('/home.html')
 def home():
     index = getpopularstocks(['^IXIC', '^GSPC', '^DJI'], '1d', '1m')
-    stocks = getpopularstocks(['NFLX', 'AAPL', 'AMC', 'GME'], '1d', '1m')
+    ticker = ['NFLX', 'AAPL', 'AMC', 'GME']
+
+    if current_user.is_authenticated:
+        for x in Pref.query.filter_by(username = current_user.username).all():
+            ticker.append(x.stockticker)
+   
+    stocks = getpopularstocks(ticker, '1d', '1m')
     news = getnews()
-    return render_template('home.html', majorindexes = index, stocklists = stocks, stocknews = news)
+    return render_template('home.html', majorindexes = index, stocklists = stocks, stocknews = news, status = current_user.is_authenticated)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/resources.html')
+def resources():
+    return render_template('resources.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True, port = 0)
